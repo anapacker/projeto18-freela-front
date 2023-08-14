9ce7fc7f66e3ec26d07ceab69a84970f26bbc17b
anapacker <anaamaria.packer@gmail.com>
2023-08-13 22:30:07 -0300

    página de login implementada

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import SignupPage from "./pages/signup/SignupPage"
 import LoginPage from "./pages/login/LoginPage"
 import HomePage from "./pages/home/HomePage"
+import { useState } from "react"
+import DataContextProvider from "./context/DataContextProvider"
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"))
+  const contextValue = { token, setToken }
 
   return (
-    <>
+    <DataContextProvider.Provider value={contextValue}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/home-page" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
           {/* <Route path="/products" element={<ProductPage />} />  */}
         </Routes>
       </BrowserRouter>
-    </>
+    </DataContextProvider.Provider>
   )
 }
 
--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -2,12 +2,14 @@ import { Link, useNavigate } from "react-router-dom";
 import Container from "../../components/FormComponents/Container";
 import Form from "../../components/FormComponents/Form";
 import Input from "../../components/FormComponents/input";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
+import DataContextProvider from "../../context/DataContextProvider";
 
 export default function LoginPage() {
-    const [formData, setFormData] = useState({ email: '', password: '' })
+    const [formData, setFormData] = useState({ email: "", password: "" })
     const [isDisable, setIsDisable] = useState(false)
+    const { setToken } = useContext(DataContextProvider)
     const navigate = useNavigate()
 
     function loginToAccont(e) {
@@ -15,18 +17,20 @@ export default function LoginPage() {
         setIsDisable(true)
 
         const promise = axios.post(`${import.meta.env.VITE_API_URL}/signin`, formData)
-        promise.then((response) => {
-            setIsDisable(false)
-
-            login(response.data)
-            navigate("/home-page")
+        promise.then((res) => {
+            localStorage.setItem("token", res.data.token)
+            setToken(res.data.token)
+            navigate("/home")
         })
-        promise.catch(() => {
+        promise.catch((res) => {
+            console.log(res)
+            if (res.response.status === 401) {
+                alert(`Senha inválida`)
+            } else {
+                alert(res.response.data)
+            }
             setIsDisable(false)
-
-            alert('Erro, tente novamente.')
         })
-
     }
 
     return (
